23bf9bde877cd55cb334bf55d7a18685dbded530
root <root@diwou-VirtualBox.(none)>
2017-12-22 00:53:04 +0100

    initial commit

---
+++ userspace/public/include/cms_version.h
@@ -0,0 +1,6 @@
+/* this is an automatically generated file.  Do not modify. */
+/* modify version.make at the top. */
+#ifndef _CMS_VERSION_H_
+#define _CMS_VERSION_H_
+#define CMS_RELEASE_VERSION "GE31-412CTL-C01_R02"
+#endif
